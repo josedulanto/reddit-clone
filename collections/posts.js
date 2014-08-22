f77b0221e73b1c0f93d76fa18071dc5bc8afaabd
José Dulanto <jose.dulanto@live.com>
2014-08-22 12:52:33 -0500

    adds new post validation, also adds parameter sanitizing and automatic submission of created_at and updated_at fields

--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,25 @@
 Posts = new Meteor.Collection('posts');
+
+Meteor.methods({
+  post: function(postAttributes) {
+    postWithSameLink = Posts.findOne({url: postAttributes.url});
+
+    // ensure the post has a title
+    if (!postAttributes.title)
+      throw new Meteor.Error(422, 'Please complete the fields');
+
+    // check that there are no previous posts with the same link
+    if (postAttributes.url && postWithSameLink)
+      throw new Meteor.Error(302, 'This URL has already been posted', postWithSameLink._id);
+
+    // pick out the whitelisted keys
+    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+      created_at: new Date().getTime(),
+      updated_at: new Date().getTime()
+    });
+
+    var postId = Posts.insert(post);
+
+    return postId;
+  }
+});
