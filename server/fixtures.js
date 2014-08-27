248782f98699931dfaf0d7d7ca7325a9859c75e7
José Dulanto <jose.dulanto@live.com>
2014-08-27 01:50:44 -0500

    updates fixtures with the addition of two users, one more post and two comments for the first post; also renames created_at and updated_at attributes to camelcase (createdAt and updatedAt), adds userId to posts

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,26 +1,71 @@
 if (Posts.find().count() === 0) {
+  
+  // Create users John Doe and John Smith
+  var doeId = Meteor.users.insert({
+    profile: { name: 'John Doe' }
+  });
+  
+  var doe = Meteor.users.findOne(doeId);
+  
+  var smithId = Meteor.users.insert({
+    profile: {name: 'John Smith'}
+  });
+  
+  var smith = Meteor.users.findOne(smithId);
+  
+  // Create posts
+  Posts.insert({
+    title: 'Amazon’s Twitch Acquisition Is Official',
+    userId: smith._id,
+    author: smith.profile.name,
+    url: 'http://techcrunch.com/2014/08/25/amazon-will-officially-acquire-twitch/',
+    votes: 0,
+    createdAt: moment(),
+    updatedAt: moment()
+  });
   Posts.insert({
     title: 'Y Combinator-Backed Traction Is A Marketplace Connecting Brands With Freelance Marketers',
-    author: 'Anthony Ha',
+    userId: doe._id,
+    author: doe.profile.name,
     url: 'http://techcrunch.com/2014/08/19/traction-launch/',
     votes: 0,
-    created_at: moment().subtract(1, 'days'),
-    updated_at: moment().subtract(1, 'days')
+    createdAt: moment().subtract(1, 'days'),
+    updatedAt: moment().subtract(1, 'days')
   });
   Posts.insert({
     title: 'Google Launches Photo Sphere Camera App On iOS',
-    author: 'Sarah Perez',
+    userId: smith._id,
+    author: smith.profile.name,
     url: 'http://techcrunch.com/2014/08/19/google-launches-photo-sphere-camera-on-ios/',
     votes: 0,
-    created_at: moment().subtract(2, 'days'),
-    updated_at: moment().subtract(2, 'days')
+    createdAt: moment().subtract(2, 'days'),
+    updatedAt: moment().subtract(2, 'days')
   });
   Posts.insert({
     title: 'This Is The Worst App In The World',
-    author: 'Alex Willhelm',
+    userId: doe._id,
+    author: doe.profile.name,
     url: 'http://techcrunch.com/2014/08/19/this-is-the-worst-app-in-the-world/',
     votes: 0,
-    created_at: moment().subtract(3, 'days'),
-    updated_at: moment().subtract(3, 'days')
+    createdAt: moment().subtract(3, 'days'),
+    updatedAt: moment().subtract(3, 'days')
+  });
+  
+  var posts = Meteor.posts.fetch();
+  
+  // Create comments
+  Comments.insert({
+    postId: posts[0]._id,
+    userId: doe._id,
+    author: doe.profile.name,
+    body: "I'm officially 'not with it'. I went to twitch, loaded a few pages and found myself watching a live stream of someone playing minecraft, as if playing minecraft wasn't bad enough. WTF.",
+    createdAt: moment().add(10, 'minutes')
+  });
+  Comments.insert({
+    postId: posts[0]._id,
+    userId: smith._id,
+    author: smith.profile.name,
+    body: "That's why I prefer Diablo 3... FTW!",
+    createdAt: moment().add(13, 'minutes')
   });
 }
